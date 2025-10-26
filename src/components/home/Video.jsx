b6c03dae47a561f5d0eb1a413e2fc252b967afe6
debayansaha <grindart004@gmail.com>
2025-10-26 17:27:16 +0530

    ee

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Video = () => {
   return (
     <div className='h-full w-full'>
-         <video className='h-full w-full object-cover' autoPlay loop muted src="https://download-video-ak.vimeocdn.com/v3-1/playback/36bc59b8-6671-4358-abc2-15555fc6ae59/69496b2d?__token__=st=1761475520~exp=1761479120~acl=%2Fv3-1%2Fplayback%2F36bc59b8-6671-4358-abc2-15555fc6ae59%2F69496b2d%2A~hmac=eac08540ef26f2415cdb2756d12838fb5a1d0f5812069ef8f82b922365ea1547&r=dXMtZWFzdDE%3D" ></video>
+         <video className='h-full w-full object-cover' autoPlay loop muted src="https://player.vimeo.com/progressive_redirect/playback/1119600858/rendition/1080p/file.mp4?loc=external&log_user=0&signature=c4a137161d6ce80a52c50c7ee23d4fdf8df103bfc816252fc304e317a43bacc6" ></video>
     </div>
   )
 }
